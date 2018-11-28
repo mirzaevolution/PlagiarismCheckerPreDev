2fb06bd8753df154ef437afc688e0ef7d341dc33
Mirza Ghulam Rasyid <mirzaevolution@gmail.com>
2018-11-28 16:46:06 +0700

    adding a couple sample projects

---
+++ FluentSchedulerSample/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentScheduler;
+using static System.Console;
+using System.Threading;
+
+namespace FluentSchedulerSample
+{
+    public class MyJob : IJob
+    {
+        public void Execute()
+        {
+            WriteLine($"....<<HELLO WORLD>>");
+        }
+    }
+    public class My2ndJob : IJob
+    {
+        public void Execute()
+        {
+            WriteLine($"....<<<HELLO MIRZA>>>");
+        }
+    }
+    class Program
+    {
+        static void Sample1()
+        {
+            Registry registry = new Registry();
+            registry.Schedule<MyJob>()
+                .AndThen<My2ndJob>().WithName("__0x123__")
+                .ToRunNow().AndEvery(5).Seconds();
+            JobManager.JobStart += JobManager_JobStart;
+            JobManager.JobEnd += JobManager_JobEnd;
+            JobManager.Initialize(registry);
+            ReadLine();
+            JobManager.Stop();
+
+        }
+
+        private static void JobManager_JobEnd(JobEndInfo obj)
+        {
+            WriteLine($"....Task [{obj.Name}] stopped at {DateTime.Now} with duration of {obj.Duration.TotalSeconds} seconds\n\n");
+
+        }
+
+        private static void JobManager_JobStart(JobStartInfo obj)
+        {
+            WriteLine($"....Task [{obj.Name}] started at {obj.StartTime}");
+        }
+
+        static void Main(string[] args)
+        {
+            Sample1();
+            ReadLine();
+        }
+    }
+}
---
+++ HangFireSample/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hangfire;
+namespace HangFireSample
+{
+    class Program
+    {
+        private static void ConfigureServer()
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage(@"data source=.\MSSQL16; initial catalog=HangfireV1; integrated security=SSPI;");
+        }
+        private static void FireAndForget()
+        {
+
+            using (BackgroundJobServer server = new BackgroundJobServer())
+            {
+                BackgroundJob.Enqueue(() => Console.WriteLine("Hello Motherfucker!"));
+                Console.ReadLine();
+            }
+            
+        }
+        private static void DelayedStart()
+        {
+
+            using (BackgroundJobServer server = new BackgroundJobServer(new BackgroundJobServerOptions
+            {
+                ServerName = "CyberSpear",
+                SchedulePollingInterval = TimeSpan.FromSeconds(12) //default 15 sec
+            }))
+            {
+                Console.WriteLine($"....Server started at {DateTime.Now}");
+                
+                BackgroundJob.Schedule(() => Console.WriteLine($".... > Hello Motherfucker {DateTime.Now}!"), TimeSpan.FromMinutes(0.5));
+                Console.ReadLine();
+            }
+
+        }
+        private static void RecurringJobSample()
+        {
+
+            using (BackgroundJobServer server = new BackgroundJobServer(new BackgroundJobServerOptions
+            {
+                ServerName = "CyberSpear",
+                SchedulePollingInterval = TimeSpan.FromSeconds(12) //default 15 sec
+            }))
+            {
+                Console.WriteLine($"....Server started at {DateTime.Now}");
+                Console.WriteLine($"....Waiting for the job to be executed every minute");
+                string id = Guid.NewGuid().ToString();
+                RecurringJob.AddOrUpdate(id, () => Console.WriteLine($"....[{DateTime.Now}]"), Cron.Minutely());
+                Console.ReadLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            ConfigureServer();
+            //FireAndForget();
+            //DelayedStart();
+            RecurringJobSample();
+        }
+    }
+}
